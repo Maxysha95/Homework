b26edff4166dc167f6c4bd7f80f40975c6e82df7
maxysha95 <maxysha9@mail.ru>
2018-11-03 16:01:23 +0300

    "add 2nd function"

--- Task_3.py
+++ Task_3.py
@@ -1,4 +1,18 @@
 def squares(a):
-    for i in range(a):
+    for i in a:
         yield i**2
-    
+
+
+def repeatntimes(elems, n):
+    for n in range(n):
+        for i in elems:
+            yield i
+
+
+if __name__ == '__main__':
+    assert list(squares([])) == []
+    assert list(squares([1, 3, 5])) == [1, 9, 25]
+    print("squares is ok")
+
+
+    print(list(repeatntimes([1, 2, 3],0)))
